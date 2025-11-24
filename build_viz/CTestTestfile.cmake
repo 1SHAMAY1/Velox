226627337ae24aee148c19beae30decfb9f0e4e0
1SHAMAY1 <139101765+1SHAMAY1@users.noreply.github.com>
2025-11-24 14:30:40 +0530

    Initial commit - Velox Physics Engine

---
+++ build_viz/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: E:/Resume/PhysicsEngine
+# Build directory: E:/Resume/PhysicsEngine/build_viz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("tests")
+subdirs("visualizer")
---
+++ build_viz/tests/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Resume/PhysicsEngine/tests
+# Build directory: E:/Resume/PhysicsEngine/build_viz/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
